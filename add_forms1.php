9ff2fe969ef632d6bd81007761e11fcc89ee829d
andres <iasmivan@gmail.com>
2022-12-01 17:04:04 -0500

    primer comit

---
+++ add_forms1.php
@@ -0,0 +1,21 @@
+<?php
+    include("conecta.php");
+    $bd = conectar();
+
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $cedula = $_POST['cedula'];
+
+
+    $sql = "insert into hoja_de_vida values ('$nombres','$apellidos','$cedula')";
+    $res = mysqli_query($bd,$sql);
+
+    if (!$res){
+            echo json_encode('error');
+    }
+    else {
+            echo json_encode('datos registrados');
+    }
+    mysqli_close($bd);
+    
+?>
